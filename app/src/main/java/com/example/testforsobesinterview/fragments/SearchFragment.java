75bd6f3765f62a0017a42de030332a6655771940
Ilya <lobovilia1999@gmail.com>
2021-02-21 17:28:47 +0300

    Работает поиск городов.

--- app/src/main/java/com/example/testforsobesinterview/fragments/SearchFragment.java
+++ app/src/main/java/com/example/testforsobesinterview/fragments/SearchFragment.java
@@ -3,26 +3,29 @@ package com.example.testforsobesinterview.fragments;
  * author Lobov-IR
  */
 
+import android.content.Context;
 import android.os.Bundle;
-import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.testforsobesinterview.MainActivity;
 import com.example.testforsobesinterview.R;
 import com.example.testforsobesinterview.Town;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchFragment extends Fragment {
@@ -31,21 +34,48 @@ public class SearchFragment extends Fragment {
     private View rootView;
     private RecyclerView list;
     private List<Town> townList;
+    private EditText editText;
+    private TownAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_search,container,false);
+        editText = rootView.findViewById(R.id.enteredText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                Log.d("ILYA-TEG", "!!!!!!!!!!!!!!!!! " + s);
+                String currentName = editText.getText().toString();
+                List<Town> newList = new ArrayList<>();
+                for (Town town : townList){
+                    if (town.getName().contains(currentName))
+                        newList.add(town);
+                }
+                adapter = new TownAdapter(newList);
+                list.setAdapter(adapter);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         list = rootView.findViewById(R.id.list);
         list.setLayoutManager(new LinearLayoutManager(getActivity()));
-        updateUI();
+        updateUI(); // мб тут выставить галочку текущего города
         return rootView;
     }
 
     private void updateUI(){
         townList = ((MainActivity) getActivity()).getTowns();
-        TownAdapter townAdapter = new TownAdapter(townList);
-        list.setAdapter(townAdapter);
+        adapter = new TownAdapter(townList);
+        list.setAdapter(adapter);
     }
 
     /*
@@ -69,6 +99,7 @@ public class SearchFragment extends Fragment {
         public void onBindViewHolder(@NonNull TownHolder holder, int position) {
             Town town = townList.get(position);
             holder.bind(town);
+            // кажется тут выставлять галочку при создании списка.
         }
 
         @Override
@@ -88,7 +119,6 @@ public class SearchFragment extends Fragment {
         public TownHolder(LayoutInflater inflater, ViewGroup parent){
             super(inflater.inflate(R.layout.list_item,parent,false));
             itemView.setOnClickListener(this::onClick);
-
             firstLine = itemView.findViewById(R.id.firstLine);
             secondLine = itemView.findViewById(R.id.secondLine);
         }
@@ -102,12 +132,11 @@ public class SearchFragment extends Fragment {
         @Override
         public void onClick(View v) {
             firstLine.setChecked(true);
+            ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).
+                    hideSoftInputFromWindow(editText.getWindowToken(),0);   // иначе баг с открытой клавиатурой при возврате в прошлый фрагмент
             // тут пихать в базу информацию о выбраном городе, чтобы на фрагменте ее спросить.
             FragmentActivity activity = getActivity();
             MainActivity.resolveFragment(activity.getSupportFragmentManager());
         }
     }
-
-
-
 }
